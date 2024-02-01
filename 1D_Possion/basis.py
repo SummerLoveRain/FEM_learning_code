9d01c2fdd7c25b90e38d4b90f187f68ce7f5fbb8
yangqh <1157571718@qq.com>
2024-02-01 12:05:38 +0800

    Using FEM to solve 1D Possion problems.

---
+++ 1D_Possion/basis.py
@@ -0,0 +1,159 @@
+class Basis:
+    LINEAR = 'linear'
+    QUADRATIC = 'quadratic'
+    CUBIC = 'cubic'
+
+def basis_local_linear_1D(x, basis_index, derivative_order):
+    '''
+    x : coordinate
+    basis_index : 0 left, 1 right
+    derivative_order : the derivative order of some basis function
+    return : the value of a basis function with some derivate order at point x which is in [0, 1]
+    '''
+    if basis_index == 0:
+        if derivative_order == 0:
+            result = 1 - x
+        elif derivative_order == 1:
+            result = -1
+        elif derivative_order >=2 & type(derivative_order)==int:
+            result = 0
+        else:
+            ValueError("The derivative order is not correct!")
+    elif basis_index == 1:        
+        if derivative_order == 0:
+            result = x
+        elif derivative_order == 1:
+            result = 1
+        elif derivative_order >=2 & type(derivative_order)==int:
+            result = 0
+        else:
+            ValueError("The derivative order is not correct!")
+    else:        
+        ValueError("The index of basis function is not correct!")
+    return result
+
+def basis_local_quadratic_1D(x, basis_index, derivative_order):
+    '''
+    x : coordinate
+    basis_index : 0 left, 1 middle, 2 right
+    derivative_order : the derivative order of some basis function
+    return : the value of a basis function with some derivate order at point x which is in [0, 1]
+    '''
+    if basis_index == 0:
+        if derivative_order == 0:
+            result = 2*x**2 - 3*x + 1
+        elif derivative_order == 1:
+            result = 4*x - 3
+        elif derivative_order ==2:
+            result = 4
+        elif derivative_order >=3 & type(derivative_order)==int:
+            result = 0
+        else:
+            ValueError("The derivative order is not correct!")
+    elif basis_index == 1:        
+        if derivative_order == 0:
+            result = 2*x**2 - x
+        elif derivative_order == 1:
+            result = 4*x - 1
+        elif derivative_order ==2:
+            result = 4
+        elif derivative_order >=3 & type(derivative_order)==int:
+            result = 0
+        else:
+            ValueError("The derivative order is not correct!")
+    elif basis_index == 2:        
+        if derivative_order == 0:
+            result = -4*x**2 + 4*x
+        elif derivative_order == 1:
+            result = -8*x + 4
+        elif derivative_order ==2:
+            result = -8
+        elif derivative_order >=3 & type(derivative_order)==int:
+            result = 0
+        else:
+            ValueError("The derivative order is not correct!")
+    else:        
+        ValueError("The index of basis function is not correct!")
+    return result
+
+
+# def basis_local_cubic_1D(x, basis_index, derivative_order):
+#     '''
+#     x : coordinate
+#     basis_index : 0 left, 1 middle, 2 right
+#     derivative_order : the derivative order of some basis function
+#     return : the value of a hermite basis function with some derivate order at point x which is in [0, 1]
+#     '''
+#     if basis_index == 0:
+#         if derivative_order == 0:
+#             result = 2*x**3 - 3*x**2 + 1
+#         elif derivative_order == 1:
+#             result = 6*x**2 - 6*x
+#         elif derivative_order ==2:
+#             result = 12*x - 6
+#         elif derivative_order ==3:
+#             result = 12
+#         elif derivative_order >=4 & type(derivative_order)==int:
+#             result = 0
+#         else:
+#             ValueError("The derivative order is not correct!")
+#     elif basis_index == 1:        
+#         if derivative_order == 0:
+#             result = -2*x**3 + 3*x**2
+#         elif derivative_order == 1:
+#             result = -6*x**2 + 6*x
+#         elif derivative_order ==2:
+#             result = -12*x + 6
+#         elif derivative_order ==3:
+#             result = -12
+#         elif derivative_order >=4 & type(derivative_order)==int:
+#             result = 0
+#         else:
+#             ValueError("The derivative order is not correct!")
+#     elif basis_index == 2:        
+#         if derivative_order == 0:
+#             result = x**3 - 2*x**2 + x
+#         elif derivative_order == 1:
+#             result = 3*x**2 - 4*x + 1
+#         elif derivative_order ==2:
+#             result = 6*x - 4
+#         elif derivative_order ==3:
+#             result = 6
+#         elif derivative_order >=4 & type(derivative_order)==int:
+#             result = 0
+#         else:
+#             ValueError("The derivative order is not correct!")
+#     elif basis_index == 3:        
+#         if derivative_order == 0:
+#             result = x**3 - x**2
+#         elif derivative_order == 1:
+#             result = 3*x**2 - 2*x
+#         elif derivative_order ==2:
+#             result = 6*x - 2
+#         elif derivative_order ==3:
+#             result = 6
+#         elif derivative_order >=4 & type(derivative_order)==int:
+#             result = 0
+#         else:
+#             ValueError("The derivative order is not correct!")
+#     else:        
+#         ValueError("The index of basis function is not correct!")
+#     return result
+
+def affine_x(a, b, x, derivative_order):    
+    '''
+    a : the lower bound.
+    b : the upper bound.
+    x : coordinate
+    derivative_order : the derivative order of some basis function
+    return : a affine mapping value at x
+    '''
+    if derivative_order == 0:
+        result = (x-a)/(b-a)
+    elif derivative_order == 1:
+        result = 1/(b-a)
+    elif derivative_order >=2 & type(derivative_order)==int:
+        result = 0
+    else:
+        ValueError("The derivative order is not correct!")
+    return result
---
+++ 1D_Possion/main.py
@@ -0,0 +1,98 @@
+import numpy as np
+from fem import *
+from solver import Possion_1D_FE_solver_Dirichlet, Possion_1D_FE_solver_Neumann, Possion_1D_FE_solver_Robin
+from basis import Basis
+
+def Exact_u(x):
+    '''
+    解析解
+    '''
+    u = x * np.cos(x)
+    return u
+
+def Exact_u_derivative(x):
+    '''
+    解析解
+    '''
+    u = np.cos(x) - x*np.sin(x)
+    return u
+
+def coefficient_function(x):
+    coe = np.exp(x)
+    return coe
+
+# def boundary_function(a, b, x):
+#     if np.abs(x-a)<1e-15:
+#         result = 0.0
+#     elif np.abs(x-b)<1e-15:
+#         result = np.cos(b)
+#     else:
+#         ValueError('No boundary points!')
+#     return result
+
+def source_function(x):
+    sour = -np.exp(x)*(np.cos(x)-2.0*np.sin(x)-x*np.cos(x)-x*np.sin(x))
+    return sour
+
+if __name__ == '__main__':
+    a = 0.0
+    b = 1.0
+    Gaussian_Integral_N = 3
+    Nh = 6
+    
+    for i in range(Nh):
+        number_of_elements = 2**(i+2)
+        solver = Possion_1D_FE_solver_Dirichlet(a, b, coefficient_function, source_function, number_of_elements=number_of_elements, \
+                                      basis_function_type_trial=Basis.QUADRATIC, basis_function_type_test=Basis.QUADRATIC, Gaussian_Integral_N=Gaussian_Integral_N)
+        x = np.linspace(a,b,2*number_of_elements+1)
+        # test_number = 100
+        # x = np.linspace(a,b,test_number+1)
+        # x = 0.65
+        # u_pred_quadratic = solver.solve()
+        # u_pred_quadratic = solver.solve_x(x)
+        # u_true = Exact_u(x)        
+
+        # 打印误差        
+        log_str = 'n %5d L_infinity: %10.6e L2: %10.6e H1: %10.6e H1-semi: %10.6e' % (number_of_elements, solver.L_infinity_error(Exact_u), solver.L2_error(Exact_u), \
+                                                                                solver.H1_error(Exact_u, Exact_u_derivative), solver.H1_semi_error(Exact_u_derivative))
+        print(log_str)
+
+    print("\n")
+
+        
+    for i in range(Nh):
+        number_of_elements = 2**(i+2)
+        solver = Possion_1D_FE_solver_Neumann(a, b, coefficient_function, source_function, number_of_elements=number_of_elements, \
+                                      basis_function_type_trial=Basis.QUADRATIC, basis_function_type_test=Basis.QUADRATIC, Gaussian_Integral_N=Gaussian_Integral_N)
+        x = np.linspace(a,b,2*number_of_elements+1)
+        # test_number = 100
+        # x = np.linspace(a,b,test_number+1)
+        # x = 0.65
+        # u_pred_quadratic = solver.solve()
+        # # u_pred_quadratic = solver.solve_x(x)
+        # u_true = Exact_u(x)        
+
+        # 打印误差        
+        log_str = 'n %5d L_infinity: %10.6e L2: %10.6e H1: %10.6e H1-semi: %10.6e' % (number_of_elements, solver.L_infinity_error(Exact_u), solver.L2_error(Exact_u), \
+                                                                                solver.H1_error(Exact_u, Exact_u_derivative), solver.H1_semi_error(Exact_u_derivative))
+        print(log_str)
+
+    print("\n")
+    
+        
+    for i in range(Nh):
+        number_of_elements = 2**(i+2)
+        solver = Possion_1D_FE_solver_Robin(a, b, coefficient_function, source_function, number_of_elements=number_of_elements, \
+                                      basis_function_type_trial=Basis.QUADRATIC, basis_function_type_test=Basis.QUADRATIC, Gaussian_Integral_N=Gaussian_Integral_N)
+        x = np.linspace(a,b, 3*number_of_elements+1)
+        # test_number = 100
+        # x = np.linspace(a,b,test_number+1)
+        # x = 0.65
+        # u_pred_quadratic = solver.solve()
+        # u_pred_quadratic = solver.solve_x(x)
+        # u_true = Exact_u(x)        
+
+        # 打印误差  
+        log_str = 'n %5d L_infinity: %10.6e L2: %10.6e H1: %10.6e H1-semi: %10.6e' % (number_of_elements, solver.L_infinity_error(Exact_u), solver.L2_error(Exact_u), \
+                                                                                solver.H1_error(Exact_u, Exact_u_derivative), solver.H1_semi_error(Exact_u_derivative))
+        print(log_str)
